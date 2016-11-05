d8029126236946250a601a51787f0bdab40be357
Andrea R <amrdev08@gmail.com>
2016-11-05 13:51:34 -0400

    servers connected

---
+++ server.js
@@ -0,0 +1,28 @@
+  var express = require("express");
+  var PORT = process.env.PORT || 7000;
+
+
+  var app = express();
+  app.set("view engine", "ejs");
+
+  var PORT = process.env.PORT || 7000;
+
+//creating routes
+  app.get("/", function(req,res) {
+    res.render("landing");
+  });
+
+  app.get("/userData", function(req,res){
+    var userData = [
+      {name: "Creek", image: "http://cdn.thepondguy.com/images/homepage/pond-lake-promo.jpg"},
+      {name: "Mountain View", image: "http://pop.h-cdn.co/assets/cm/15/05/54c83083b00c9_-_pmx0606pond006_large.jpg"},
+      {name: "Pond", image: "http://openwalls.com/image/264/small_river_1920x1200.jpg"}
+      
+    ]
+    res.render("userData", {userData: userData});
+        
+  });
+
+  app.listen(PORT, function(){
+    console.log('App listening on PORT ' + PORT);
+  });
