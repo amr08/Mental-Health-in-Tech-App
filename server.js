a4d79a54f7100a607487d349f1958bee0bd2c671
Andrea R <amrdev08@gmail.com>
2016-11-07 14:47:16 -0500

    added controllers

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
   var express = require("express");
+  var bodyParser = require('body-parser');
+  var methodOverride = require('method-override');
+
   var PORT = process.env.PORT || 7000;
 
 
   var app = express();
+
   app.use(express.static(__dirname + '/public'));
+
   app.set("view engine", "ejs");
 
-  var PORT = process.env.PORT || 7000;
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
 
-  
+  app.use(methodOverride('_method'));
 
-//creating routes
-  app.get("/", function(req,res) {
-    res.render("landing");
-  });
 
-  app.get("/userData", function(req,res){
-    var userData = [
-      {name: "Creek", image: "http://cdn.thepondguy.com/images/homepage/pond-lake-promo.jpg"},
-      {name: "Mountain View", image: "http://pop.h-cdn.co/assets/cm/15/05/54c83083b00c9_-_pmx0606pond006_large.jpg"},
-      {name: "Pond", image: "http://openwalls.com/image/264/small_river_1920x1200.jpg"}
-      
-    ]
-    res.render("userData", {userData: userData});
-        
-  });
+  // var sequelizeConnection = models.sequelize;
+
+  // sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
+
+// //syncing tabels
+//  .then(function(){
+
+//     return sequelizeConnection.sync({force:true})
+
+//   })
+
+
+var routes = require('./controllers/project_controller.js');
+  app.use('/', routes);
 
   app.listen(PORT, function(){
     console.log('App listening on PORT ' + PORT);
