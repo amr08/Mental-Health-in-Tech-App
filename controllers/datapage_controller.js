ca68fd99cbb08fb145e9d531b4d4d0fb29992f35
Andrea R <amrdev08@gmail.com>
2016-11-16 17:29:40 -0500

    big push

--- controllers/datapage_controller.js
+++ controllers/datapage_controller.js
@@ -10,7 +10,7 @@
 
 //Route for countries/states
 
- router.get("/:country", function(req,res){
+ router.get("/search", function(req,res){
  	Data.find({}, function(err, data) {
  		if(err){
  			console.log("ERROR!");
@@ -19,7 +19,7 @@
 
  	});
     // var zip = req.params.zip;
-
+     res.render("country");
   
   });
 
